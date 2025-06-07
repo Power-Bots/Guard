1f09975a9904d045fa3f5e87e913de272281578e
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2025-06-07 00:28:05 -0400

    `mute.role` config option

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
-import { bot } from "@power-bots/powerbotlibrary"
+import { bot, Config } from "@power-bots/powerbotlibrary"
 export { bot } from "@power-bots/powerbotlibrary"
 export { knex } from "@power-bots/powerbotlibrary"
-
 import { Timer } from "./lib/timers"
+import { Guild, NonThreadGuildBasedChannel, Role, Snowflake } from "discord.js"
 
 bot.setup(__dirname)
 bot.run()
+
+Config.onSet("guild.mute.role", async (config: any) => {
+    let guild: Guild | null = null
+    let role: Role | null = null
+    try {
+        guild = await bot.client.guilds.fetch(config.id)
+    } catch {}
+    if (!guild) return
+    try {
+        role = await guild.roles.fetch(config.value)
+    } catch {}
+    if (!role) return
+    let channels = await guild.channels.fetch()
+    channels.forEach((channel: NonThreadGuildBasedChannel | null, id: Snowflake) => {
+        channel?.permissionOverwrites.edit(role, {
+            SendMessages: false,
+            SendMessagesInThreads: false,
+            CreatePublicThreads: false,
+            CreatePrivateThreads: false
+        })
+    })
+})
 
 // Check for finished timers every 1 second
 async function timerCheck() {
